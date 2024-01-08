9c48d224921a5fff3fab1571afa4ca9fb76064f3
anapaulagarfias <paulagarfias4@gmail.com>
2024-01-08 16:22:39 -0600

    Ejercicios 1 y 2 realizados

--- variables_js.js
+++ variables_js.js
@@ -1,28 +1,46 @@
 
 /** Ejercicio para esta sesion
  * 
+ * EJERCICIO 1
  * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
  * Pueden variar entre las palabras reservadas var, let y const
  *      -number
  *      -string
- *      -boolean
+ *      -boolean 
+ */
 
- * 
- * 
- * Declarar variables numericas y realizar las operaciones aritmeticas basicas
+//SOLUCIÓN 
+
+var nombreCompleto = "Ana Paula";
+var age = 27;
+var apellidoPaterno = "Garfias"
+var apellidoMaterno = "Rios"
+let postreFavorito = "Helado";
+let vivo = true;
+let hombre = false;
+const NUMERO_TELEFONICO = 4431697064;
+const NUMERO_CASA = 514;
+const NUMERO_EMPLEADO = 3092809;
+
+ /* Declarar variables numericas y realizar las operaciones aritmeticas basicas
  *      -suma
  *      -resta
  *      -multiplicacion
  *      -division
- * 
- * Declarar variables que concatenen cadenas de texto
+ */
+
+ /* Declarar variables que concatenen cadenas de texto
  *      -5 mensajes diferentes en pantalla, sin limite de palabras.
- * 
- * Declarar 5 variables y convertirlas a otro tipo de dato
- * 
- *  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
+ */
+
+ /* Declarar 5 variables y convertirlas a otro tipo de dato
+ */
+
+ 
+
+ 
+ /*  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
  * Imprimir los resultados en pantalla con un console.log()
  */
 
-let nombre = "Alberto"
-let number = 33
+
