d8bb6cf6a323e5df5ebbfc8075513a2b77f3ac00
anapaulagarfias <paulagarfias4@gmail.com>
2024-01-09 11:47:24 -0600

    Se crea documento para ejercicio de funciones

---
+++ funciones_js.js
@@ -0,0 +1,14 @@
+/**Ejercicios para esta sesion
+ * 
+ * Declara una funcion para cada problema planteado
+ * 
+ * calcular el perimetro de un circulo
+ * calcular el area de un rectangulo
+ * calcular el cuadrado de un numero
+ * calcular la conversion de grados celsius a farenheit
+ * calcular el valor del voltaje dadas la resistencia y la corriente
+ * calcular el volumen de una esfera
+ * 
+ * CONSIDERACIONES: poner en practica las mejores practicas para declarar funciones
+ * Los ejercicios se entregan tanto en funciones con nombre como funciones flecha
+ */
