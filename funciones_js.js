ee93689f50d911796ff89c6c1e7a512696585492
anapaulagarfias <paulagarfias4@gmail.com>
2024-01-09 16:08:43 -0600

    Ejercicio de funciones

--- funciones_js.js
+++ funciones_js.js
@@ -1,14 +1,30 @@
 /**Ejercicios para esta sesion
  * 
  * Declara una funcion para cada problema planteado
- * 
- * calcular el perimetro de un circulo
- * calcular el area de un rectangulo
- * calcular el cuadrado de un numero
- * calcular la conversion de grados celsius a farenheit
- * calcular el valor del voltaje dadas la resistencia y la corriente
- * calcular el volumen de una esfera
- * 
- * CONSIDERACIONES: poner en practica las mejores practicas para declarar funciones
+ */
+
+//calcular el perimetro de un circulo
+
+function perimetroCirculo(d){
+    return p = 3.14 * d;
+};
+
+ //calcular el area de un rectangulo
+
+ function areaRectangulo(a, b){
+    console.log(r = b * a);
+};
+
+ //calcular el cuadrado de un numero
+
+ function cuadradoNumero(n){
+    return `El cuadrado de ${n} es ${n = n**2}`;
+};
+
+ //calcular la conversion de grados celsius a farenheit
+ //calcular el valor del voltaje dadas la resistencia y la corriente
+ //calcular el volumen de una esfera
+ 
+ /** CONSIDERACIONES: poner en practica las mejores practicas para declarar funciones
  * Los ejercicios se entregan tanto en funciones con nombre como funciones flecha
  */
