e4984c52fd04110ffe05ca5e59475208fafae680
anapaulagarfias <paulagarfias4@gmail.com>
2024-01-09 17:14:54 -0600

    Se empezó el ejercicio de controlDeFlujo

---
+++ controlDeFlujo_js.js
@@ -0,0 +1,141 @@
+// Ejercicios para esta sesion
+
+/** Ejercicio 1
+ * 
+ * Elabora un programa que determine si una persona tiene edad
+ * suficiente para votar
+ * 
+ * Debe recibir como parametro la edad
+ * 
+ * Consideraciones: utilizar la estructura if-else
+ */
+
+let edad = 3
+
+let edadParaVotar = edad >=18 ? "Puedes votar":"No puedes votar";
+console.log(edadParaVotar);
+
+/** Ejercicio 2
+ * 
+ * Desarrolla un programa que permita obtener el promedio de un/a alumno/a
+ * recibiendo como parametro 4 calificaciones
+ * 
+ * Si el promedio es aprobatorio (mayor o igual a 7) imprimir aprobado
+ * en el caso de que no sea un promedio aprobatorio imprimir reporobado
+ * 
+ * Consideraciones: utilizar la estructura if-else
+ * 
+ */
+
+/** Ejercicio 4
+ * 
+ * Desarrolla un programa que evalue si un numero es divisible
+ * entre 7 y 8
+ * 
+ * Debe recibir como parametro un numero, cual sea
+ * 
+ * Si se cumple, imprimir verdadero
+ * Si no, imprimir falso
+ * 
+ * 
+ * Consideracion - Utilizar la estructura if-else
+ * Casos de prueba: 56, 7, 8, 0, 224, 73
+ */
+
+/** Ejercicio 5
+ * 
+ * Desarrolla un programa que evalue si un numero es divisible
+ * entre 4 o 9
+ * 
+ * Debe recibir como parametro un numero aleatorio
+ * 
+ * Si se cumple, imprimir verdadero
+ * Si no, imprimir falso
+ * 
+ * Consideracion - Utilizar la estructura if-else
+ */
+
+/** Ejercicio 6
+ * 
+ * Crear un programa que funcione como una calculadora simple
+ * que realice las operaciones aritmeticas basicas
+ * 
+ *  -suma
+ *  -resta
+ *  -multiplicacion
+ *  -division
+ * 
+ * Debe recibir como parametros la operacion a realizar y dos numeros
+ * Dependiendo de la opcion que se indique, realizar la operacion correspondiente
+ * 
+ * 1. suma
+ * 2. resta
+ * 3. multiplicacion
+ * 4. division
+ * 
+ * Consideraciones: utilizar la estructura switch
+ */
+
+/** Ejercicio 7
+ * 
+ * Realizar un programa que reciba como parametro una categoria de peliculas
+ * 
+ * -accion
+ * -drama
+ * -comedia
+ * -romance
+ * -suspenso
+ * -terror
+ * 
+ * Segun sea la categoria que se reciba, imprimir una recomendacion de pelicula
+ * de acuerdo al parametro recibido
+ * 
+ * Consideraciones: utilizar la estructura switch
+ */
+
+
+/** Ejercicio 8
+ * 
+ * Crear una interfaz de un cajero ATM
+ * 
+ * Debe recibirse como parametro alguna de las siguientes opciones
+ * 
+ * 1. Retirar dinero
+ * 2. Transferencia
+ * 3. Deposito
+ * 4. Pago de servicios
+ * 
+ * Segun sea la opcion indicada, imprimir en pantalla la accion a realizar
+ * Utilizar if/else
+ */
+
+/** Ejercicio 9
+ * 
+ * Escribir en codigo un programa conversor de divisas
+ * 
+ * Debe recibir como parametro una cantidad en pesos mexicanos y
+ * Segun sea la opcion que se indique, realizar la conversion correspondiente
+ * 
+ * 1. A dolares estadounidenses
+ * 2. A euros 
+ * 3. A yenes japoneses
+ * 4. A libra esterlina
+ * 5. A franco suizo
+ * 
+ * Consideracion: estructura switch
+ * 
+ */
+
+/** Ejercicio 10
+ * 
+ * Realizar un programa que calcule un descuento del 10% a un producto si se ingresa el codigo
+ * de descuento correcto
+ * 
+ * Debe recibir como parametros el precio del producto
+ * y el codigo de descuento
+ * 
+ * Sugerencia de codigo de descuento DESCUENTO10
+ * 
+ * Consideracion: utilizar la estructura if-else
+ * 
+ */
