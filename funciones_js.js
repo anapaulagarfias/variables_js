9be250279366391dc48997bb7bdefcbfbb02a457
anapaulagarfias <paulagarfias4@gmail.com>
2024-01-09 16:49:39 -0600

    Eejercicio de funciones, terminado

--- funciones_js.js
+++ funciones_js.js
@@ -22,8 +22,16 @@ function perimetroCirculo(d){
 };
 
  //calcular la conversion de grados celsius a farenheit
+
+const celsiusAFarenheit = (c) => (`${c}°C es igual a ${(c * 9/5)+32}°F`);
+
  //calcular el valor del voltaje dadas la resistencia y la corriente
+
+ const voltaje = (r, c) => console.log("El voltaje es igual a " + r*c + "V");
+
  //calcular el volumen de una esfera
+
+ const volumenEsfera = (r) => (`El volumen de la esfera es de ${4/3 * 3.14 * r**3}`);
  
  /** CONSIDERACIONES: poner en practica las mejores practicas para declarar funciones
  * Los ejercicios se entregan tanto en funciones con nombre como funciones flecha
